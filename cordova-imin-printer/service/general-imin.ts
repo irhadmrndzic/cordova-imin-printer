002ea11edb645460bfa1bab7c3c2601aa69b26bd
Irhad Mrndžić <irhad.mrndzic@gmail.com>
2022-04-10 23:53:39 +0200

    Added service functions

---
+++ cordova-imin-printer/service/general-imin.ts
@@ -0,0 +1,49 @@
+// import { defer } from 'rxjs/observable/defer';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import * as _ from 'lodash';
+// declare var iminPrinter: any;
+
+// @Injectable()
+// export class GeneralIminProvider {
+
+  // constructor() {
+  // }
+
+  // printText(text): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.printText(text));
+    // });
+  // }
+
+  // checkStatus(): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.checkStatus());
+    // });
+  // }
+
+  // setTextStyleBold(): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.setTextStyleBold());
+    // });
+  // }
+
+  // printBitmap(text): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.printBitmap(text));
+    // });
+  // }
+
+  // initPrinter(): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.initPrinter());
+    // });
+  // }
+
+  // printAndFeedPaper(value): Observable<any> {
+    // return defer(() => {
+      // return Observable.fromPromise(iminPrinter.printAndFeedPaper(value));
+    // });
+  // }
+
+// }
